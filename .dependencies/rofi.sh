4c048db9061d0bea90b0d23f9ef09849126fafb3
Chris Soper <ChrisSoper1@gmail.com>
2016-05-04 13:53:41 -0400

    added wallpaper + configs, update tmux.conf

---
+++ .dependencies/rofi.sh
@@ -0,0 +1,1 @@
+apt-get install libglib2.0-dev libxinerama-dev libxft-dev libpango1.0-dev libpangocairo-1.0-0 libcairo2-dev libx11-dev libstartup-notification0-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb1-dev libxcb-xkb-dev libxcb-xinerama0-dev libx11-xcb-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-icccm4-dev
