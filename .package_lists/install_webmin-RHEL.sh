40aa0ff19a58806f2313bf3cd5984558ec8d36e8
Chris Soper <ChrisSoper1@gmail.com>
2016-05-16 12:04:40 -0400

    fixed dead link in webmin script

--- .package_lists/install_webmin-RHEL.sh
+++ .package_lists/install_webmin-RHEL.sh
@@ -2,8 +2,8 @@
 cat > /etc/yum.repos.d/webmin.repo<<EOF
 [Webmin]
 name=Webmin Distribution Neutral
-#baseurl=http://download.webmin.com/download/yum
-mirrorlist=http://download.webmin.com/download/yum/mirrorlist
+baseurl=http://download.webmin.com/download/yum
+#mirrorlist=http://download.webmin.com/download/yum/mirrorlist
 enabled=1
 EOF
 
